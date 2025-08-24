1e47a0fa7b09bca8db2e07cbc6e17c31dbc82fa0
Clay Speakman <clay@rockethub.ai>
2025-08-24 19:40:57 -0400

    Remove Unused Platform Import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { Platform } from 'react-native';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 import TestingModeProvider from '../components/test/TestingModeProvider';
 import { AuthProvider } from '../hooks/useAuth';
