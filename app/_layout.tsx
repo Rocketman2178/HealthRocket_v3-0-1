433d2df31109e5b8312f5918c73cf6eabaf91171
Clay Speakman <clay@rockethub.ai>
2025-08-24 19:36:23 -0400

    Fix Platform Import Error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { Platform } from 'react-native';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 import TestingModeProvider from '../components/test/TestingModeProvider';
 import { AuthProvider } from '../hooks/useAuth';
